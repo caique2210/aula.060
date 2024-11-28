eaae2eb7296091ce4d43d5435da9bcafa4732e18
caique2210 <caiquesalmaso@gmail.com>
2024-11-28 00:57:55 +0000

    Atualização Semana 09

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -12,7 +12,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
+app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -20,6 +20,7 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -30,6 +31,7 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -39,21 +41,31 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField('Role:', choices=[
+        ('Administrator', 'Administrator'),
+        ('Moderator', 'Moderator'),
+        ('User', 'User')
+    ], validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -61,16 +73,22 @@ def index():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            role = Role.query.filter_by(name=form.role.data).first()
+            if role is None:
+                role = Role(name=form.role.data)
+                db.session.add(role)
+            user = User(username=form.name.data, role=role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
         else:
             session['known'] = True
         session['name'] = form.name.data
+        session['role'] = form.role.data
         return redirect(url_for('index'))
-
-    # Recupera a lista de usuários registrados
     users = User.query.all()
+    roles = Role.query.all()
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False), users=users)
+                           known=session.get('known', False),
+                           users=users, roles=roles)
+
